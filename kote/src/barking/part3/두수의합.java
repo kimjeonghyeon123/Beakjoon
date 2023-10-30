506740713e78c64b85d3ea2b712c01b80be4f187
kimjeonghyeon <gojhkim123456@gmail.com>
2023-10-30 22:35:36 +0900

    추가

--- kote/src/barking/part3/두수의합.java
+++ kote/src/barking/part3/두수의합.java
@@ -11,25 +11,24 @@ public class 두수의합 {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
-        for (int i=0;i<n;i++){
+        for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         int x = Integer.parseInt(br.readLine());
 
-        int count=0;
-        for(int i =0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                int sum = arr[i] + arr[j];
+        Arrays.sort(arr);
 
-                if(sum == x) {
-                    count++;
-                }
-                else if(sum < x) {
-                    break;
-                }
+        int left = 0, right = n - 1, count = 0;
+        while(left < right) {
+            int sum = arr[left] + arr[right];
+            if(sum == x) {
+                left++;
+                right--;
+                count++;
             }
+            else if(sum < x) {left++;}
+            else {right--;}
         }
-
         bw.write(String.valueOf(count));
         bw.flush();
         bw.close();
