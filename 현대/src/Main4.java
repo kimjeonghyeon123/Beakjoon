1452d145a961a5707f23eca05b03a0be35942f3e
kimjeonghyeon <gojhkim123456@gmail.com>
2023-11-20 22:44:38 +0900

    추가

---
+++ 현대/src/Main4.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main4 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+
+        LinkedList<LinkedList<String>> list = new LinkedList<>();
+        for(int i = 0; i < N; i++) {
+            String[] str = br.readLine().split(" ");
+            for(int j = 0; j < N; j++) {
+                list.add();
+            }
+        }
+    }
+}
